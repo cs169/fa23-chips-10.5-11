be81e9381875a73da8c7e6f47f6ff93df55e2131
Patrick Cui <cuiboy@rayjay.TKE-HOUSE>
2023-11-20 23:56:41 -0800

    more tests

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -83,4 +83,5 @@ describe Representative do
     result = described_class.civic_api_to_representative_params(@mock_info)
     expect(result[0].photo_url).to eq 'http://example.com/photo.jpg'
   end
+
 end
