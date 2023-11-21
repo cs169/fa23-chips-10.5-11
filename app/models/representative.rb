b056bc38291235994c9864bd2ee0c9b081836806
Patrick Cui <cuiboy@rayjay.TKE-HOUSE>
2023-11-20 23:12:10 -0800

    refactor

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,32 +1,41 @@
 # frozen_string_literal: true
-
 class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
 
   def self.civic_api_to_representative_params(rep_info)
-    reps = []
-    rep_info.officials.each_with_index do |official, index|
-      ocdid_temp = ''
-      title_temp = ''
-      rep_info.offices.each do |office|
-        if office.official_indices.include? index
-          title_temp = office.name
-          ocdid_temp = office.division_id
-        end
+    rep_info.officials.each_with_index.map do |official, index|
+      process_official(official, index, rep_info)
+    end.compact
+  end
+
+  def self.process_official(official, index, rep_info)
+    ocdid_temp, title_temp = find_office_info(index, rep_info)
+    address = official.address.is_a?(Array) ? official.address[0] : official.address
+    return unless address
+
+    create_or_find_rep(official, ocdid_temp, title_temp, address)
+  end
+
+  def self.find_office_info(index, rep_info)
+    ocdid_temp = ''
+    title_temp = ''
+    rep_info.offices.each do |office|
+      if office.official_indices.include? index
+        title_temp = office.name
+        ocdid_temp = office.division_id
       end
-      address = official.address.is_a?(Array) ? official.address[0] : official.address
-      next unless address
-      rep_hash = {
-        name:      official.name, ocdid:     ocdid_temp,
-        title:     title_temp, address:   address.line1,
-        city:      address.city, state:     address.state,
-        zip:       address.zip, party:     official.party,
-        photo_url: official.photo_url
-      }
-      rep ||= Representative.find_by(rep_hash)
-      rep ||= Representative.create!(rep_hash) unless rep
-      reps.push(rep)
     end
-    reps
+    [ocdid_temp, title_temp]
+  end
+
+  def self.create_or_find_rep(official, ocdid_temp, title_temp, address)
+    rep_hash = {
+      name:      official.name, ocdid:     ocdid_temp,
+      title:     title_temp, address:   address.line1,
+      city:      address.city, state:     address.state,
+      zip:       address.zip, party:     official.party,
+      photo_url: official.photo_url
+    }
+    Representative.find_by(rep_hash) || Representative.create!(rep_hash)
   end
 end
