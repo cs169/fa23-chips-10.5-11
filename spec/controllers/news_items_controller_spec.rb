d872580e45eccacc54fd141b2b7e2e75da710960
Patrick Cui <cuiboy@rayjay.TKE-HOUSE>
2023-12-02 01:27:35 -0800

    trying

--- spec/controllers/news_items_controller_spec.rb
+++ spec/controllers/news_items_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe NewsItemsController, type: :controller do
+  let(:representative) { Representative.create!(name: 'John Doe') }
   let(:valid_attributes) {
-    { title: 'Title', description: 'Description', link: 'http://example.com', representative_id: 1, issue: 'Free Speech' }
+    { title: 'Title', description: 'Description', link: 'http://example.com', representative_id: representative.id, issue: 'Free Speech' }
   }
 
   let(:invalid_attributes) {
@@ -11,7 +12,7 @@ RSpec.describe NewsItemsController, type: :controller do
 
   describe "GET #index" do
     it "returns a success response" do
-      get :index, params: { representative_id: 1 }
+      get :index, params: { representative_id: representative.id }
       expect(response).to be_successful
     end
   end
@@ -19,7 +20,7 @@ RSpec.describe NewsItemsController, type: :controller do
   describe "GET #show" do
     it "returns a success response" do
       news_item = NewsItem.create! valid_attributes
-      get :show, params: {id: news_item.to_param, representative_id: 1}
+      get :show, params: {id: news_item.to_param, representative_id: representative.id}
       expect(response).to be_successful
     end
   end
