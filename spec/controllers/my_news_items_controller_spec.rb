7eac9fc5637d55c70c87596fb764445a7fb94b12
Patrick Cui <patrickcui@berkeley.edu>
2023-12-02 07:06:51 +0000

    tmp

--- spec/controllers/my_news_items_controller_spec.rb
+++ spec/controllers/my_news_items_controller_spec.rb
@@ -1,39 +1,41 @@
+# frozen_string_literal: true
+
 # spec/controllers/my_news_items_controller_spec.rb
 require 'rails_helper'
 
 RSpec.describe MyNewsItemsController, type: :controller do
-  let(:valid_attributes) {
+  let(:valid_attributes) do
     { title: 'Test Title', link: 'http://test.com', representative_id: 1 }
-  }
+  end
 
-  let(:invalid_attributes) {
+  let(:invalid_attributes) do
     { title: nil, link: nil, representative_id: nil }
-  }
+  end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new NewsItem" do
-        expect {
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new NewsItem' do
+        expect do
           post :create, params: { news_item: valid_attributes }
-        }.to change(NewsItem, :count).by(1)
+        end.to change(NewsItem, :count).by(1)
       end
 
-      it "redirects to the created news_item" do
+      it 'redirects to the created news_item' do
         post :create, params: { news_item: valid_attributes }
         expect(response).to redirect_to(NewsItem.last)
       end
     end
 
-    context "with invalid params" do
-      it "does not create a new NewsItem" do
-        expect {
+    context 'with invalid params' do
+      it 'does not create a new NewsItem' do
+        expect do
           post :create, params: { news_item: invalid_attributes }
-        }.to change(NewsItem, :count).by(0)
+        end.not_to change(NewsItem, :count)
       end
 
       it "re-renders the 'new' template" do
         post :create, params: { news_item: invalid_attributes }
-        expect(response).to render_template("new")
+        expect(response).to render_template('new')
       end
     end
   end
