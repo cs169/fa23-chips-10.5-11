273784dba82eccd5eaaabadc8f9f9792321f0b45
Connor Von Winckelmann <fervidacademic@berkeley.edu>
2023-12-02 05:52:05 +0000

    [feat] Add routes and controller.

---
+++ app/controllers/campaign_finance_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CampaignFinanceController < ApplicationController
+  def index
+    p "Hello, world!"
+  end
+
+  def search
+    p "Hello, world!"
+  end
+end
