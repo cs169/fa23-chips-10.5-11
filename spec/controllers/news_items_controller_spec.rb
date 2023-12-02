ec00b50c6427e96fba128ac1240d930b510216ff
Patrick Cui <patrickcui@berkeley.edu>
2023-12-02 09:30:04 +0000

    fixed code

--- spec/controllers/news_items_controller_spec.rb
+++ spec/controllers/news_items_controller_spec.rb
@@ -1,28 +1,30 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe NewsItemsController, type: :controller do
   let(:representative) { Representative.create!(name: 'John Doe') }
-  let(:valid_attributes) {
-    { title: 'Title', description: 'Description', link: 'http://example.com', representative_id: representative.id, issue: 'Free Speech' }
-  }
+  let(:valid_attributes) do
+    { title: 'Title', description: 'Description', link: 'http://example.com', representative_id: representative.id,
+issue: 'Free Speech' }
+  end
 
-  let(:invalid_attributes) {
+  let(:invalid_attributes) do
     { title: '', description: '', link: '', representative_id: '', issue: '' }
-  }
+  end
 
-  describe "GET #index" do
-    it "returns a success response" do
+  describe 'GET #index' do
+    it 'returns a success response' do
       get :index, params: { representative_id: representative.id }
       expect(response).to be_successful
     end
   end
 
-  describe "GET #show" do
-    it "returns a success response" do
+  describe 'GET #show' do
+    it 'returns a success response' do
       news_item = NewsItem.create! valid_attributes
-      get :show, params: {id: news_item.to_param, representative_id: representative.id}
+      get :show, params: { id: news_item.to_param, representative_id: representative.id }
       expect(response).to be_successful
     end
   end
 end
-
